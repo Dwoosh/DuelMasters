b1a7b4fbbe64229db49a3125e9ece81b5918bfdc
Dwoosh <34167272+Dwoosh@users.noreply.github.com>
2019-07-03 22:38:53 +0200

    upgraded project to 2019.1.8f1

---
+++ Assets/Resources/Scripts/MenuScripts/SwitchCanvasScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCanvasScript : MonoBehaviour
+{
+    public Canvas MainMenu;
+    public Canvas CardListMenu;
+
+    public void SwitchToMainMenu()
+    {
+        CardListMenu.enabled = false;
+        MainMenu.enabled = true;
+    }
+
+    public void SwitchToCardListMenu()
+    {
+        MainMenu.enabled = false;
+        CardListMenu.enabled = true;
+    }
+
+}
